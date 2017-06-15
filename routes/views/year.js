f2893a199b455e7a5eac89cb8c0268c9b3b5d965
Sidstumple <cydstumpel@MacBook-Pro-2.local>
2017-06-15 16:14:23 +0200

    fixed loading problem

--- routes/views/year.js
+++ routes/views/year.js
@@ -11,48 +11,65 @@ exports = module.exports = function(req, res) {
     course: req.params.courseType
   };
 
-  locals.data = {
-    year: [],
-    blok: [],
-    courseType: [],
-    course: []
-  };
+  // locals.data = {
+  //   year: [],
+  //   courseType: [],
+  //   course: []
+  // };
 
   // Load curriculum
-  view.on('init', function(next) {
-    var q = keystone.list('Curriculum').model.findOne({
+  // view.on('init', function(next) {
+  //   var q = keystone.list('Curriculum').model.findOne({
+  //     slug: locals.filters.year
+  //   });
+  //
+  //   q.exec(function(err, result) {
+  //     locals.data.year = result;
+  //     console.log(result, 'year');
+  //   });
+  //   var blok = keystone.list('Course').model.find({
+  //     blok: locals.filters.course
+  //   });
+  //
+  //   blok.exec(function(err, result) {
+  //     locals.data.course = result;
+  //     console.log(result, 'blok');
+  //   });
+  //
+  //   var courseType = keystone.list('CourseType').model.find();
+  //
+  //   courseType.exec(function(err, result) {
+  //     locals.data.courseType = result;
+  //     console.log(result, 'courseType');
+  //   });
+  //
+  //    var courses = keystone.list('Course').model.find({
+  //      category: locals.filters.course,
+  //      year: locals.filters.year
+  //    });
+  //
+  //    courses.exec(function(err, result) {
+  //      locals.data.course = result;
+  //      console.log(result, 'courses');
+  //      next(err);
+  //    })
+  // });
+
+  view.query('year', keystone.list('Curriculum').model.findOne({
       slug: locals.filters.year
-    });
+    }));
 
-    q.exec(function(err, result) {
-      locals.data.year = result;
-      next(err);
-    });
+  view.query('blok', keystone.list('Course').model.find({
+    year: locals.filters.year,
+    blok: locals.filters.course
+  }));
 
-    var blok = keystone.list('Course').model.find({
-      blok: locals.filters.course
-    });
+  view.query('courseType', keystone.list('CourseType').model.find());
 
-    blok.exec(function(err, result) {
-      locals.data.blok = result;
-    });
-
-    var courseType = keystone.list('CourseType').model.find();
-
-    courseType.exec(function(err, result) {
-      locals.data.courseType = result;
-    });
-
-     var courses = keystone.list('Course').model.find({
-       category: locals.filters.course,
-       year: locals.filters.year
-     });
-
-     courses.exec(function(err, result) {
-       locals.data.course = result;
-       console.log(locals.data.course);
-     })
-  });
+  view.query('course', keystone.list('Course').model.find({
+    category: locals.filters.course,
+    year: locals.filters.year
+  }));
 
   // Render view
   view.render('year');
